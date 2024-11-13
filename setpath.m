225e360974645e4551e49c4ee66c0ef54e0f59e6
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2024-11-13 19:05:03 +0900

    Reverted

---
+++ setpath.m
@@ -0,0 +1,16 @@
+addpath('./scripts');
+addpath('./livescripts');
+addpath('./data');
+addpath('./2019before/common');
+addpath('./2019before/Sec03');
+addpath('./2019before/Sec04');
+addpath('./2019before/Sec05');
+addpath('./2019before/Sec06');
+addpath('./2019before/Sec07');
+addpath('./2019before/Sec08');
+addpath('./2019before/Sec09');
+addpath('./2019before/Sec10');
+addpath('./Appendix');
+if exist('./tmp','dir')~=7
+    mkdir('./tmp')
+end
